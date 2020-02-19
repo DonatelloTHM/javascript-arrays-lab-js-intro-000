431a6f2258365b4faf11eb3aa4ac67772d73b944
Donat Pllana <donat.pllana@outlook.com>
2020-02-19 00:19:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 
 // Add your functions and code here
-function destructivelyAppendKitten(name){
-  var z;
-  return z=(...name,'kitten');
-}
+function destructivelyAppendKitten(name)
